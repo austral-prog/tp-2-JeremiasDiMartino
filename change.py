cab3e6961768873213c2b9e55a41b05f3fe74bbe
JeremiasDiMartino <jdimartino@mail.austral.edu.ar>
2025-04-04 10:56:14 -0400

    Update change.py

--- change.py
+++ change.py
@@ -1,9 +1,9 @@
 def change():
     gasto = 23.75
     dinero_recibido = 100
-    print("Ingresar gasto: ")
+    print("Ingresar gasto:")
     print(gasto)
-    print("Dinero recibido ")
+    print("Dinero recibido")
     print(dinero_recibido)
     vuelto = dinero_recibido - gasto
     pesos = int(vuelto)
@@ -12,4 +12,3 @@ def change():
     print("\nVuelto\n")
     print(f"Pesos:\n{pesos}")
     print(f"Centavos:\n{centavos}")
-
